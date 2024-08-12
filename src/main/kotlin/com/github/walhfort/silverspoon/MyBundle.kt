a064b1f465fb45474a37f09d8e7f56fe87f0e6d2
GitHub Action <action@github.com>
2024-08-12 12:17:29 +0000

    Template cleanup

--- src/main/kotlin/com/github/walhfort/silverspoon/MyBundle.kt
+++ src/main/kotlin/com/github/walhfort/silverspoon/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.walhfort.silverspoon
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/walhfort/silverspoon/services/MyProjectService.kt
+++ src/main/kotlin/com/github/walhfort/silverspoon/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.walhfort.silverspoon.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.walhfort.silverspoon.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
